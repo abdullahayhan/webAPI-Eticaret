8c88203bf5494208051b834f01525d2e6c4bd3f4
abdullahayhan <aayhaan4@gmail.com>
2022-11-21 20:55:39 +0300

    Proje dosyası ekle.

---
+++ src/API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetSingleProduct(int id)
+        {
+            return "single product";
+        }
+    }
+}
