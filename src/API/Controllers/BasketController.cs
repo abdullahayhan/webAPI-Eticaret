af69f6f285d58198862bca5be4774f4619491e4b
abdullahayhan <aayhaan4@gmail.com>
2022-12-04 18:16:12 +0300

    BasketController eklendi.

---
+++ src/API/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using API.Core.DbModels;
+using API.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await basketRepository.GetBasketAsync(id);
+            return Ok(basket ?? new CustomerBasket(id)); 
+            // basketi gönder eğer yoksa bir tane customerBasket oluştur idsini ver gönder.
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var updated = await basketRepository.UpdateBasketAsync(basket);
+            return Ok(updated);
+        }
+
+
+        [HttpDelete]
+        public async Task DeleteBasketAsync(string id)
+        {
+            await basketRepository.DeleteBasketAsync(id);
+        }
+
+    }
+}
