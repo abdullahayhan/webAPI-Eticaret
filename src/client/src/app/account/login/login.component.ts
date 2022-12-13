4e7dc54e166f79f09bcc5793bafc2b7e083f2d55
abdullahayhan <aayhaan4@gmail.com>
2022-12-14 01:35:29 +0300

    login islemleri

--- src/client/src/app/account/login/login.component.ts
+++ src/client/src/app/account/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { validate } from 'uuid';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  
+  
+  loginForm : FormGroup;
+
+  constructor(private accountService:AccountService){}
+  
+  ngOnInit(){
+    this.createLoginForm();
+  }
+
+
+  createLoginForm(){
+    this.loginForm = new FormGroup({
+      email : new FormControl('',Validators.required),
+      password : new FormControl('',Validators.required)
+    });
+  }
+
+  onSubmit(){
+    this.accountService.login(this.loginForm.value).subscribe(()=>{
+      console.log('user logged in');
+    }, error=>{
+      console.log('hata : ',error);
+    });
+  }
+
 
 }
